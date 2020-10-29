b1e3da47b87f3661b2064dafaae55f6339eb289b
DiscoverSquishy <26318936+DiscoverSquishy@users.noreply.github.com>
2020-10-29 21:21:24 +0300

    Reformatted Docker IP Assignment

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 cd /home/container || exit
 
 # Make internal Docker IP address available to processes.
-export INTERNAL_IP="$(ip route get 1 | awk '{print $NF;exit}')"
+INTERNAL_IP="$(ip route get 1 | awk '{print $NF;exit}')"
+export INTERNAL_IP
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
