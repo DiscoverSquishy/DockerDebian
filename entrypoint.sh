39d117bcbfc9ab13cbf7d7cd05642fd2b2c22fb0
DiscoverSquishy <26318936+DiscoverSquishy@users.noreply.github.com>
2020-10-29 21:19:29 +0300

    Fixed entrypoint linting

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 cd /home/container || exit
 
 # Make internal Docker IP address available to processes.
-export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+export INTERNAL_IP="$(ip route get 1 | awk '{print $NF;exit}')"
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(eval echo $(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-# Setup script for different Docker enviroments.
-
-# -----------------------------------------------------
-# Author: DiscoverSquishy || https://discoversquishy.me
-# Discord: DiscoverSquishy#0001
-# Email: noaimi2214@gmail.com
-# -----------------------------------------------------
