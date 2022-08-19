1af35ad1cfb94d91dc42c4171299f282c42f1e8a
DiscoverSquishy <26318936+DiscoverSquishy@users.noreply.github.com>
2022-08-19 12:21:43 +0300

    Added required dependancies

--- scripts/temurin-download.sh
+++ scripts/temurin-download.sh
@@ -2,13 +2,13 @@
 
 JAVA_VERSION=${1}
 
-aarch64=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK'$JAVA_VERSION'U-jdk_aarch64_linux_hotspot" | sed '/^$/d' | sed 's/"//g')
-ppc64el=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK'$JAVA_VERSION'U-jdk_ppc64le_linux_hotspot" | sed '/^$/d' | sed 's/"//g')
-amd64=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK'$JAVA_VERSION'U-jdk_x64_linux_hotspot" | sed '/^$/d' | sed 's/"//g')
+aarch64=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK"$JAVA_VERSION"U-jdk_aarch64_linux_hotspot" | sed '/^$/d' | sed 's/"//g')
+ppc64el=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK"$JAVA_VERSION"U-jdk_ppc64le_linux_hotspot" | sed '/^$/d' | sed 's/"//g')
+amd64=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK"$JAVA_VERSION"U-jdk_x64_linux_hotspot" | sed '/^$/d' | sed 's/"//g')
 
 
 ARCH="$(dpkg --print-architecture)"; \
-    case "${ARCH}" in \
+    case ${ARCH} in \
        aarch64|arm64) \
          BINARY_URL="$aarch64"; \
          ;; \
