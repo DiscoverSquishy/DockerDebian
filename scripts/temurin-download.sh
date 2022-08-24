dd2689388d0955f6c0968a36999d43bdecf08dca
DiscoverSquishy <26318936+DiscoverSquishy@users.noreply.github.com>
2022-08-24 13:27:33 +0300

    Fixed Temurin Download Logic

--- scripts/temurin-download.sh
+++ scripts/temurin-download.sh
@@ -2,26 +2,33 @@
 
 JAVA_VERSION=${1}
 
-aarch64=$(curl -s 'https://api.github.com/repos/adoptium/temurin'$JAVA_VERSION'-binaries/releases/latest' | grep -wo 'https.*' | grep -m1 'OpenJDK'$JAVA_VERSION'U-jdk_aarch64_linux_hotspot' | sed '/^$/d' | sed 's/"//g')
-ppc64el=$(curl -s 'https://api.github.com/repos/adoptium/temurin'$JAVA_VERSION'-binaries/releases/latest' | grep -wo 'https.*' | grep -m1 'OpenJDK'$JAVA_VERSION'U-jdk_ppc64le_linux_hotspot' | sed '/^$/d' | sed 's/"//g')
-amd64=$(curl -s 'https://api.github.com/repos/adoptium/temurin'$JAVA_VERSION'-binaries/releases/latest' | grep -wo 'https.*' | grep -m1 'OpenJDK'$JAVA_VERSION'U-jdk_x64_linux_hotspot' | sed '/^$/d' | sed 's/"//g')
-
+aarch64=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK"$JAVA_VERSION"U-jdk_aarch64_linux_hotspot" | sed 's/.$//')
+arm=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK"$JAVA_VERSION"U-jdk_arm_linux_hotspot" | sed 's/.$//')
+ppc64el=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK"$JAVA_VERSION"U-jdk_ppc64le_linux_hotspot" | sed 's/.$//')
+s390x=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK"$JAVA_VERSION"U-jdk_s390x_linux_hotspot" | sed 's/.$//')
+amd64=$(curl -s "https://api.github.com/repos/adoptium/temurin$JAVA_VERSION-binaries/releases/latest" | grep -wo 'https.*' | grep -m1 "OpenJDK"$JAVA_VERSION"U-jdk_x64_linux_hotspot" | sed 's/.$//')
 
 ARCH="$(dpkg --print-architecture)"; \
-    case "${ARCH}" in \
-       aarch64|arm64) \
-         BINARY_URL="$aarch64"; \
-         ;; \
-       ppc64el|powerpc:common64) \
-         BINARY_URL="$ppc64el"; \
-         ;; \
-       amd64|i386:x86-64) \
-         BINARY_URL="$amd64"; \
-         ;; \
-       *) \
+  case "${ARCH}" in \
+    aarch64|arm64) \
+      BINARY_URL="$aarch64"; \
+      ;; \
+    armhf|arm) \
+      BINARY_URL="$arm"; \
+      ;; \
+    ppc64el|powerpc:common64) \
+      BINARY_URL="$ppc64el"; \
+      ;; \
+    s390x|s390:64-bit) \
+      BINARY_URL="$s390x"; \
+      ;; \
+    amd64|i386:x86-64) \
+      BINARY_URL="$amd64"; \
+      ;; \
+  *) \
          echo "Unsupported arch: ${ARCH}"; \
          exit 1; \
          ;; \
-    esac;
+  esac;
 
-curl -LfsSo /tmp/openjdk.tar.gz ${BINARY_URL}
+curl -LfsSo /tmp/openjdk.tar.gz "${BINARY_URL}"
